7597f577c7f311fa0ce759b2a572d6a0acf34914
sagorsikder <mdsagorsikder97@gmail.com>
2022-11-10 02:19:10 +0600

    review show in my review

--- index.js
+++ index.js
@@ -44,9 +44,30 @@ async function run(){
 
 
         app.post('/review',async (req,res)=>{
-            const order = req.body;
-            const result = await reviewCollection.insertOne(order);
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review);
+            console.log(result)
             res.send(result)
+
+        })
+
+
+        app.get('/myreview',async(req,res)=>{
+
+           
+            
+            let query = {};
+            
+            if(req.query.email){
+                query = {
+                    email : req.query.email
+                }
+            }
+
+            console.log(req.query.email)
+            const cursor = reviewCollection.find(query)
+            const review = await cursor.toArray()
+            res.send(review)
 
         })
     }
