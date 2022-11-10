6706f05d23d412c54873a83f9e078f277d3b6d1f
sagorsikder <mdsagorsikder97@gmail.com>
2022-11-10 22:54:50 +0600

    almost completed

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json())
 
 
 
-
+// mongo db connenction
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.edl5qg1.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
